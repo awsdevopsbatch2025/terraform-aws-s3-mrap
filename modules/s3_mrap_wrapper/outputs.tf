21bedc96878cb02c5597a53a93dd69d4ccac59ad
awsdevopsbatch2025 <awsdevops95117@gmail.com>
2025-12-03 13:26:08 +0530

    Create outputs.tf

---
+++ modules/s3_mrap_wrapper/outputs.tf
@@ -0,0 +1,29 @@
+output "primary_bucket_name" {
+description = "Name of the primary S3 bucket."
+value = module.primary_bucket.bucket_name
+}
+
+output "primary_bucket_arn" {
+description = "ARN of the primary S3 bucket."
+value = module.primary_bucket.bucket_arn
+}
+
+output "dr_bucket_name" {
+description = "Name of the DR S3 bucket."
+value = module.dr_bucket.bucket_name
+}
+
+output "dr_bucket_arn" {
+description = "ARN of the DR S3 bucket."
+value = module.dr_bucket.bucket_arn
+}
+
+output "replication_iam_role_arn" {
+description = "ARN of the IAM role for DR bucket replication."
+value = module.dr_bucket.replication_iam_role_arn
+}
+
+output "mrap_alias" {
+description = "MRAP alias/URL (hostname) if MRAP is enabled."
+value = var.enable_mrap ? module.dr_bucket.mrap_alias : null
+}
